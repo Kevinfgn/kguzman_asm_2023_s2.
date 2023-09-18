63aa39739566ead31c65ac5b3a8b6faf158aed02
Kevinfgn <31755700+Kevinfgn@users.noreply.github.com>
2023-09-18 00:15:39 -0600

    Documentacion Ejercicio 4

--- Proyecto#1/Ejercicio#4.py
+++ Proyecto#1/Ejercicio#4.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt
 
-# Parámetros de la señal senoidal
+#Parámetros de la señal seno
 amplitud = 1.0
-frecuencia = 5.0  # Hz
-duracion = 1.0  # segundos
+frecuencia = 5.0  #Hz
+duracion = 1.0  #segundos
 muestras_por_segundo = 1000
 num_muestras = int(duracion * muestras_por_segundo)
 
@@ -14,18 +14,19 @@ tiempo = np.linspace(0, duracion, num_muestras)
 senal_senoidal = amplitud * np.sin(2 * np.pi * frecuencia * tiempo)
 
 # Agregar ruido gaussiano
-amplitud_ruido = 0.1
+amplitud_ruido = 0.1 #Dependiendo del ruido se van a necesitar varias integrales o derivdas para reconstruir la señal
 ruido = amplitud_ruido * np.random.randn(num_muestras)
 senal_con_ruido = senal_senoidal + ruido
 
 # Derivar la señal
 derivada = np.gradient(senal_con_ruido, tiempo)
+
 # Aplicar integración compleja para reconstruir la señal original
-senal_reconstruida = np.cumsum(derivada) / muestras_por_segundo
+señal_reconstruida = np.cumsum(derivada) / muestras_por_segundo
 
 
 #Deriva e integra hasta obtener una señal reconstruida
-segunda_derivada = np.gradient(senal_reconstruida,tiempo)
+segunda_derivada = np.gradient(señal_reconstruida,tiempo)
 señal_reconstruida_dos = np.cumsum(segunda_derivada)/muestras_por_segundo
 tercera_derivada = np.gradient(señal_reconstruida_dos,tiempo)
 señal_reconstruida_tres = np.cumsum(tercera_derivada)/muestras_por_segundo
@@ -41,22 +42,22 @@ señal_reconstruida_7 = np.cumsum(septim_derivada)/muestras_por_segundo
 # Aplicar un filtro pasa bajos para eliminar componentes de alta frecuencia
 cutoff_frequency = 10.0  # Hz
 b, a = butter(4, cutoff_frequency / (muestras_por_segundo / 2), 'low')
-senal_filtrada = filtfilt(b, a, senal_reconstruida)
+señal_filtrada = filtfilt(b, a, señal_reconstruida)
 
 # Visualizar la señal senoidal original, la señal con ruido, la señal reconstruida y la señal filtrada
 plt.figure(figsize=(12, 8))
 plt.subplot(4, 1, 1)
 plt.plot(tiempo, senal_senoidal)
-plt.title('Señal Senoidal Original')
+plt.title('Señal Seno Original')
 plt.subplot(4, 1, 2)
 plt.plot(tiempo, senal_con_ruido)
 plt.title('Señal con Ruido Gaussiano')
 plt.subplot(4, 1, 3)
-plt.plot(tiempo, senal_reconstruida)
-plt.title('Señal Reconstruida Derivada 1')
+plt.plot(tiempo, señal_filtrada)
+plt.title('Señal Reconstruida por un Filtro Pasa Bajas')
 plt.subplot(4, 1, 4)
 plt.plot(tiempo, señal_reconstruida_7)
-plt.title('Señal Reconstruida Derivada 7')
+plt.title('Señal Reconstruida en la septima Derivada ')
 plt.tight_layout()
 plt.show()
 
